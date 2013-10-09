b72563f9eb1c4824fb2262a2dac8ae22aa364728
victorCouto <victorcouto3@gmail.com>
2013-10-09 13:38:01 -0300

    teste

---
+++ DistanceConverter.Tests/TestDistanceConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DistanceConverter.Tests
+{
+    [TestFixture]
+    public class TestDistanceConverter
+    {
+        [Test]
+        public void TestConverter()
+            {
+            DistanceConverter converter = new DistanceConverter();
+            Assert.AreEqual(8.04672, converter.FromMilesToKm(5), 0.001d);
+        }
+
+    }
+}
---
+++ DistanceConverter/DistanceConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DistanceConverter
+{
+    public class DistanceConverter
+    {
+        public double FromMilesToKm(double miles)
+        {
+            return miles * 1.609344;
+        }
+
+
+        public double FromKMtoMiles(double km)
+        {
+            return km / 1.609344;
+        }
+
+        public void OtherConverter()
+        {
+            double unusedvariable = 11.55;
+        }
+
+
+       
+    }
+}
